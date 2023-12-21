3b86e58b814236d1a8e8ef4afa9821cc98b0f07b
farid saboor <134936000+faridsaboor1@users.noreply.github.com>
2023-12-21 15:57:43 +0330

    edit css address

--- view/contact_me_tpl.php
+++ view/contact_me_tpl.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>admin page</title>
-    <link rel="stylesheet" href="../assets/css/fontawesome-free-6.5.1-web/fontawesome-free-6.5.1-web/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/fontawesome-free-6.5.1-web/fontawesome-free-6.5.1-web/css/all.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-free-6.5.1-web/fontawesome-free-6.5.1-web/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-free-6.5.1-web/fontawesome-free-6.5.1-web/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../assets/css/admin_style/index_2.css">
+    <link rel="stylesheet" href="assets/css/admin_style/index_2.css">
 </head>
 <body>
     
@@ -80,6 +80,6 @@
     </form>
 <!-- input contact end -->
 
-    <script src="../assets/js/script.js"></script>
+    <script src="assets/js/script.js"></script>
 </body>
 </html>
